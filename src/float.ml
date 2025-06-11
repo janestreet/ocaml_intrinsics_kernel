e27ddc4e47b2c7a208ff0fb4756b4e48764c025c
public-release <as-public-release@janestreet.com>
2025-06-11 19:18:58 +0100

    v0.18~preview.130.36+188

--- src/float.ml
+++ src/float.ml
@@ -4,7 +4,7 @@ external min
   -> (float[@unboxed])
   @@ portable
   = "caml_sse2_float64_min_bytecode" "caml_sse2_float64_min"
-[@@noalloc] [@@builtin] [@@no_effects] [@@no_coeffects]
+[@@noalloc] (* [@@builtin] *) [@@no_effects] [@@no_coeffects]
 
 external max
   :  (float[@unboxed])
@@ -12,7 +12,7 @@ external max
   -> (float[@unboxed])
   @@ portable
   = "caml_sse2_float64_max_bytecode" "caml_sse2_float64_max"
-[@@noalloc] [@@builtin] [@@no_effects] [@@no_coeffects]
+[@@noalloc] (* [@@builtin] *) [@@no_effects] [@@no_coeffects]
 
 module Unboxed = struct
   external min
@@ -21,7 +21,7 @@ module Unboxed = struct
     -> (float#[@unboxed])
     @@ portable
     = "caml_sse2_float64_min_bytecode" "caml_sse2_float64_min"
-  [@@noalloc] [@@builtin] [@@no_effects] [@@no_coeffects]
+  [@@noalloc] (* [@@builtin] *) [@@no_effects] [@@no_coeffects]
 
   external max
     :  (float#[@unboxed])
@@ -29,5 +29,5 @@ module Unboxed = struct
     -> (float#[@unboxed])
     @@ portable
     = "caml_sse2_float64_max_bytecode" "caml_sse2_float64_max"
-  [@@noalloc] [@@builtin] [@@no_effects] [@@no_coeffects]
+  [@@noalloc] (* [@@builtin] *) [@@no_effects] [@@no_coeffects]
 end
